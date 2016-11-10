4f31b1343cfa40353c746b1e4dc4792ba09d500e
Song Chen <schen2315@bths.edu>
2016-11-10 13:46:11 -0500

    added tester

--- tester.cpp
+++ tester.cpp
@@ -1,6 +1,34 @@
+#include <vector>
 #include "BST.h"
+
+#define nodes 10
 using namespace std;
 int main() {
-	BST<int> tree; 
+	srand(time(NULL));
+	//test insert
+	{	
+		int arr[nodes];
+		int maxVal = 100;
+		//primitive type
+		BST<int> tree1;
+		for(int i=0; i < nodes; i++) {
+			int val = rand() % maxVal;
+			//tree1.insert(val);
+			arr[i] = val;
+		}
+		sort(arr, arr + nodes);
+
+		//check that arr and tree1.display() match 
+		//inorder traversal of tree1 is same as sorted array
+		for(int i=0; i < nodes; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		//tree1.display();
+		//type containing pointers
+		BST<vector<int>> tree2;
+
+		//passing a reference to an object on the heap
+	}
 	return 0;
 }
